a61ccf9c980fda9e7b650095c79a87fd962e979a
Lucas Migge <lucas.migge@picpay.com>
2025-04-06 13:00:14 -0300

    adding mySQL persistence

--- src/main/java/com/overseer/controller/HomeController.java
+++ src/main/java/com/overseer/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.overseer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Wellcome to Overseer";
+    }
+
+    @GetMapping("/greetings/{name}")
+    public String greetings(@PathVariable String name) {
+        return "Hello, " + name + "!" + " Welcome to Overseer!";
     }
 }
